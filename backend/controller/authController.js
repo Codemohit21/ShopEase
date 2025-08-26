3a1e77288f6cac2f972c73c29bb299587c7f867a
Codemohit21 <123625047+Codemohit21@users.noreply.github.com>
2025-08-27 02:25:37 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -65,17 +65,13 @@ export const login = async (req,res) => {
 }
 export const logOut = async (req,res) => {
 try {
-    res.clearCookie("token",{
-        httpOnly:true,
-        secure:true,
-        sameSite: "none"
-    })
+    res.clearCookie("token")
     return res.status(200).json({message:"logOut successful"})
 } catch (error) {
     console.log("logOut error")
-    return res.status(500).json({message:LogOut error ${error}})
+    return res.status(500).json({message:`LogOut error ${error}`})
 }
-    
+    
 }
 
 
